cda0132d6b53f17b32e0611c2149642e20db52d1
yjkim0109 <yjkkim0109@gmail.com>
2022-09-22 10:35:26 +0900

    feat: search 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,31 +1,53 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 
 function App() {
 	const [Id, setId] = useState('');
 	const [Name, setName] = useState('');
 	const [Img, setImg] = useState('');
+	const [Num, setNum] = useState('');
+	const input = useRef(null);
 
 	const searchPokemon = async () => {
 		let response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${Id}`);
 		console.log(response);
+		setNum(response.data.id);
 		setName(response.data.name);
 		setImg(response.data.sprites.front_default);
 	};
 
+	const reset = () => {
+		input.current.value = '';
+	};
+
 	useEffect(() => {
 		searchPokemon();
-	}, [Id]);
+	}, []);
+
+	const onChange = (e) => {
+		setId(e.target.value);
+	};
+
+	const search = () => {
+		searchPokemon();
+		reset();
+	};
 
 	return (
 		<>
 			<h1>search your pokemon</h1>
 			<label htmlFor='search'>poke-id:</label>
-			<input type='text' placeholder='id' id='search' />
-			<button>search pokemon</button>
+			<input
+				onChange={onChange}
+				type='text'
+				placeholder='id'
+				id='search'
+				ref={input}
+			/>
+			<button onClick={search}>search pokemon</button>
 
 			<div>
-				<h2>#{Id}</h2>
+				<h2>#{Num}</h2>
 				<img src={Img} alt={Name} />
 				<h3>{Name}</h3>
 			</div>
