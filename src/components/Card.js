6db65695f932b12cfd545577bf9fe7fdf3366479
yjkim0109 <yjkkim0109@gmail.com>
2022-09-22 16:59:00 +0900

    Design: Card 스타일링 추가

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,14 @@
 function Card({ Num, Name }) {
 	return (
-		<div>
-			<h2>#{Num}</h2>
+		<div className='Card'>
+			<h2>
+				<span>#{Num}</span>
+				{Name}
+			</h2>
 			<img
 				src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${Num}.png`}
 				alt={Name}
-				style={{ width: 300, height: 300 }}
 			/>
-			<h3>{Name}</h3>
 		</div>
 	);
 }
